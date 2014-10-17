644ffc52f3992478b89071594b9fa0288ad712b6
Palabola <hodobalazs@gmail.com>
2014-10-17 11:54:00 +0200

    Update settings.cmake

--- cmake/compiler/gcc/settings.cmake
+++ cmake/compiler/gcc/settings.cmake
@@ -22,7 +22,7 @@ add_definitions(-DHAVE_SSE2 -D__SSE2__)
 message(STATUS "GCC: SFMT enabled, SSE2 flags forced")
 
 if( WITH_WARNINGS )
-  set(WARNING_FLAGS "-W -Wall -Wextra -Winit-self -Winvalid-pch -Wfatal-errors")
+  set(WARNING_FLAGS "-W -Wall -Wextra -Winit-self  -Wfatal-errors")
   set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${WARNING_FLAGS}")
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${WARNING_FLAGS} -Woverloaded-virtual")
   message(STATUS "GCC: All warnings enabled")
